5499bb5a0bbbfd509b464649353b721d24971935
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-07-11 00:47:57 +0600

    API data extracted

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const https = require("https");
 const app = express();
 
 app.get('/', function (req, res){
-    res.send('Bismillah');
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Tongi&appid=ee112f9614166746b534d124ea0cdb53&units=metric";
+    https.get(url, function (response){
+        response.on("data", function (data){
+            const weatherdata = JSON.parse(data);
+            const temp = weatherdata.main.temp;
+            const weatherDescription = weatherdata.weather[0].description;
+            const icon = weatherdata.weather[0].icon;
+            const iconURL = "http://openweathermap.org/img/wn/" +icon+ "@2x.png"
+            console.log(icon);
+            res.write('<h1>Temperature: ' + temp + ' degree Celcius at Tongi</h1>');
+            res.write('<p>Weather Description: ' + weatherDescription + '</p>');
+            res.write('<img src=' + iconURL+ '>');
+            res.send()
+        });
+    });
 });
+
+
 app.listen(3000, function (){
     console.log('Server started at 3000');
 });
