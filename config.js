8500473c803f0b0bc0471ac7810365968d75777c
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-07-11 20:08:50 +0600

    API KEY Hidden

--- config.js
+++
@@ -1,4 +0,0 @@
-var config = {
-    MY_API_TOKEN : '',
-    SECRET_API_KEY : 'https://api.openweathermap.org/data/2.5/weather?q=Tongi&appid=ee112f9614166746b534d124ea0cdb53&units=metric',
-}
--- index.js
+++ index.js
@@ -2,30 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 const app = express();
+require('dotenv').config();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res){
     res.sendFile(__dirname + '/index.html');
-
 });
-
 app.post('/', function (req, res){
     const cityName = req.body.cityName;
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=ee112f9614166746b534d124ea0cdb53&units=metric";
+    const apiKey = process.env.API_KEY;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=" +apiKey+"&units=metric";
     https.get(url, function (response){
-    response.on("data", function (data){
-        const weatherData = JSON.parse(data);
-        const temp = weatherData.main.temp;
-        const weatherDescription = weatherData.weather[0].description;
-        const icon = weatherData.weather[0].icon;
-        const iconURL = "http://openweathermap.org/img/wn/" +icon+ "@2x.png"
-        res.write('<h1>Temperature: ' + temp + ' degree Celcius at '+ cityName +' </h1>');
-        res.write('<p>Weather Description: ' + weatherDescription + '</p>');
-        res.write('<img src=' + iconURL+ '>');
-        res.send()
+        response.on("data", function (data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const iconURL = "http://openweathermap.org/img/wn/" +icon+ "@2x.png"
+            res.write('<h1>Temperature: ' + temp + ' degree Celcius at '+ cityName +' </h1>');
+            res.write('<p>Weather Description: ' + weatherDescription + '</p>');
+            res.write('<img src=' + iconURL+ '>');
+            res.send()
+        });
     });
-});
 })
 
 
